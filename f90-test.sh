30497b119b148712edc348fe20669e55c0575fba
Gonzalo Gonzalez Abad <ggonzale@cfa.harvard.edu>
2018-04-25 13:37:42 -0400

    gga: Simplified f90 script

--- f90-test.sh
+++ f90-test.sh
@@ -5,24 +5,9 @@ gfortran -g -fbacktrace -fcheck=all -Wall -o gasbeta.exe main.o gasfit_module.o
 ./gasbeta.exe << EOF
 gasfit_ace_io.inp
 gasfit_ace_io_output.txt
-ace_zn_vis_2017_01_16_gasbeta.txt
+spec_ace_zn_vis_2016_12_19.txt
 test_spectrum_output.txt
 sao2010_solref_air_380_480.dat
-sao2010_solref_air_380_480.dat
-newkpno_afgl.ring.bro
-  1.d12, f, 1.d0
-o3_221.vac.new.bro
-  1.d21, f, 1.d0
-o3_241.vac.new.bro
-  1.d21, f, 1.d0
-no2_220_iasb.vac.bro
-  1.d18, f, 1.d0
-bro228.10cm.bro
-  1.d17, f, 1.d0
-oclolt.dat.bro
-  1.d18, f, 1.d0
-O4_294K_BISA.BrO
-  1.d5, f, 1.d0
 res.bro228_10cm.boas
   1.d0, f, 1.d0
 EOF
