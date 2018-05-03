18b13138e11d0c115ba8d21ff33a465650c135bd
Gonzalo Gonzalez Abad <ggonzale@cfa.harvard.edu>
2018-05-03 14:40:52 -0400

    gga: Clean up f90-test.sh script and set up optimized compiling flags.

--- f90-test.sh
+++ f90-test.sh
@@ -1,8 +1,9 @@
-gfortran -g -c -fbacktrace -fcheck=all -Wall gasfit_module.f90 main.f90
-gfortran -g -fbacktrace -fcheck=all -Wall -o gasbeta.exe main.o gasfit_module.o
-
-#gfortran -O3 -c gasfit_module.f90 main.f90
-#gfortran -O3 -o gasbeta.exe main.o gasfit_module.o
+# Compilation for development:
+#gfortran -g -c -fbacktrace -fcheck=all -Wall gasfit_module.f90 main.f90
+#gfortran -g -fbacktrace -fcheck=all -Wall -o gasbeta.exe main.o gasfit_module.o
+# Compilation for production
+gfortran -O3 -c gasfit_module.f90 main.f90
+gfortran -O3 -o gasbeta.exe main.o gasfit_module.o
 
 
 ./gasbeta.exe << EOF
@@ -14,8 +15,6 @@ gasfit_ace_radiance_fitting_results.txt
 gasfit_ace_spectrum_fit_residual_output.txt
 spec_ace_zn_vis_2016_12_19.txt
 sao2010_solref_air_380_480.dat
-res.bro228_10cm.boas
-  1.d0, f, 1.d0
 EOF
 
 rm -f *.o *.mod gasbeta.exe
