394b9f3c3a8533b647b1f9c7ed8716266d425caf
Gonzalo Gonzalez Abad <ggonzale@cfa.harvard.edu>
2018-05-03 17:09:27 -0400

    gga: Add manual and examples

--- f90-test.sh
+++ f90-test.sh
@@ -8,12 +8,12 @@ gfortran -O3 -o gasbeta.exe main.o gasfit_module.o
 # Running the program
 ./gasbeta.exe << EOF
 gasfit.inp
-gasfit_ace_io_output.txt
-gasfit_ace_geolocation_for_fitting_results.txt
-gasfit_ace_solar_fitting_results.txt
-gasfit_ace_radiance_fitting_results.txt
-gasfit_ace_spectrum_fit_residual_output.txt
-spec_ace_zn_vis_2016_12_28.txt
+Output_file1.txt
+Output_file2.txt
+Output_file3.txt
+Output_file4.txt
+Output_file5.txt
+Input_file1.txt
 sao2010_solref_air_380_480.dat
 EOF
 
