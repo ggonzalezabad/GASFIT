36029425098b87dea913d875aeff15e941312ff3
Gonzalo Gonzalez Abad <ggonzale@cfa.harvard.edu>
2018-05-03 15:59:05 -0400

    gga: Final initial version of running script

--- f90-test.sh
+++ f90-test.sh
@@ -5,16 +5,17 @@
 gfortran -O3 -c gasfit_module.f90 main.f90
 gfortran -O3 -o gasbeta.exe main.o gasfit_module.o
 
-
+# Running the program
 ./gasbeta.exe << EOF
-gasfit_ace_io.inp
+gasfit.inp
 gasfit_ace_io_output.txt
 gasfit_ace_geolocation_for_fitting_results.txt
 gasfit_ace_solar_fitting_results.txt
 gasfit_ace_radiance_fitting_results.txt
 gasfit_ace_spectrum_fit_residual_output.txt
-spec_ace_zn_vis_2016_12_19.txt
+spec_ace_zn_vis_2016_12_28.txt
 sao2010_solref_air_380_480.dat
 EOF
 
+# Removing compilation by-products
 rm -f *.o *.mod gasbeta.exe
